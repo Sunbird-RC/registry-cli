f4488c1406418734e60a7943a0abea91428dcb0c
Sreejit-K <kanjarla.sreejit@gmail.com>
2024-06-13 12:51:55 +0530

    Configuration changes for release v2.0.1

--- src/config/config.ts
+++ src/config/config.ts
@@ -75,7 +75,7 @@ const auxiliary_services: AuxiliaryServices = {
 // change the verions of the registries based on the release
 const versionsAvailable: VersionManager = {
 	'v1.0.0': 'v1.0.1',
-	'v2.0.0': 'v2.0.0',
+	'v2.0.0': 'v2.0.1',
 }
 
 const definationsManagers = {
